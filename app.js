627e05870bd216f365924c1132ad2530f3440fc0
Morris-Desktop <morteza.hazrati@gmail.com>
2021-02-13 17:45:00 +1100

    backend for three end points created

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("hi");
+
+const express = require("express");
+const database = require("./services/fakeLotService");
+
+console.log(database.getLot("11YVCHAR002"));
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/api/breakdown/:breakdownType/:lotCode", (req, res) => {
+  console.log(req.params);
+  const lotCode = req.params.lotCode;
+  const breakdownType = req.params.breakdownType;
+
+  res.send(JSON.stringify(database.getLot(lotCode, breakdownType)));
+});
+
+app.listen(3000, () => console.log("Listening to port 3000 ..."));
